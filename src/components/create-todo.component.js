1a5a0064ce8bafefdce76bf4d93efb2afb2f559f
ibrahimahmedkhan <ibrahimahmed.ayesha@gmail.com>
2022-09-24 20:55:25 +0500

    Added forms and functionality for frontend

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export default class CreateTodo extends Component {
   constructor(props) {
     super(props);
+
+    this.onChangetodoDescription = this.onChangetodoDescription.bind(this);
+    this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+    this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
       todo_description: "",
@@ -44,10 +51,53 @@ export default class CreateTodo extends Component {
       todo_completed: false,
     });
   }
+
   render() {
     return (
       <div>
-        <p> Hello Create Todo</p>
+        <h3> Create new Todo</h3>
+
+        <Form onSubmit={this.onSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="text" onChange={this.onChangetodoDescription} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Responsible</Form.Label>
+            <Form.Control type="text" onChange={this.onChangeTodoResponsible} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check
+              inline
+              type="radio"
+              label="Low"
+              value="low"
+              checked={this.state.todo_priority === "low"}
+              onChange={this.onChangeTodoPriority}
+            />
+            <Form.Check
+              inline
+              type="radio"
+              label="Medium"
+              value="medium"
+              checked={this.state.todo_priority === "medium"}
+              onChange={this.onChangeTodoPriority}
+            />
+            <Form.Check
+              inline
+              type="radio"
+              label="High"
+              value="high"
+              checked={this.state.todo_priority === "high"}
+              onChange={this.onChangeTodoPriority}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Create Todo
+          </Button>
+        </Form>
       </div>
     );
   }
